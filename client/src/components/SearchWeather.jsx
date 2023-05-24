c3d64696859e6c3019a0fe634deb119426b79cc6
vaarun18 <varunsadananda108@gmail.com>
2023-05-24 16:37:57 +0530

    modified:   client/src/components/SearchWeather.jsx

--- client/src/components/SearchWeather.jsx
+++ client/src/components/SearchWeather.jsx
@@ -35,7 +35,7 @@ const SearchWeather = () => {
           }}
           onKeyDown={handleKeyDown}
         />
-        <button>
+        <button onClick={searchWeatherHandler}>
           <svg
             stroke="currentColor"
             fill="currentColor"
